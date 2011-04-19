d2b24705068c1bc4547c3a4a98f5a67dc6421f8d
francois.reynaud <francois.reynaud@800e21c8-cce7-8e7a-6c5f-8865977187ab>
2011-04-19 11:47:27 +0000

    FrancoisReynaud: bumping .NET versions

---
+++ trunk/twin/sharpclaws/server/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Simple .NET application server")]
+[assembly: AssemblyDescription("Simple .NET application server")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("eBay Inc.")]
+[assembly: AssemblyProduct("TWIN")]
+[assembly: AssemblyCopyright("Copyright eBay Inc., Twin authors, and other contributors.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7c844a11-97ec-40e0-8201-f87107175d94")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
