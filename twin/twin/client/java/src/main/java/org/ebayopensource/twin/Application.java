307e0de6f43790775952155d6213209345b76cfe
michael.palotas@gmail.com <michael.palotas@gmail.com>
2011-04-20 19:40:03 +0000

    remove previously added comments for test purposes

--- twin/twin/client/java/src/main/java/org/ebayopensource/twin/Application.java
+++ twin/twin/client/java/src/main/java/org/ebayopensource/twin/Application.java
@@ -1,7 +1,7 @@
 // [Twin] Copyright eBay Inc., Twin authors, and other contributors.
 // This file is provided to you under the terms of the Apache License, Version 2.0.
 // See LICENSE.txt and NOTICE.txt for license and copyright information.
-//
+
 
 package org.ebayopensource.twin;
 
